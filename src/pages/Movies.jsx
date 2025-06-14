52ec368b49bca69cf0951223bd7fb3ffbd37b358
Sharmasagar2 <sagarkush2003@gmail.com>
2025-06-14 21:56:45 +0530

    added Navbar

---
+++ src/pages/Movies.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Movies = () => {
+  return (
+    <div>
+     <h1 className='text-red-800'> Movies</h1>
+    </div>
+  )
+}
+
+export default Movies
---
+++ src/pages/SeatLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SeatLayout = () => {
+  return (
+    <div>
+      seatlayout
+    </div>
+  )
+}
+
+export default SeatLayout
