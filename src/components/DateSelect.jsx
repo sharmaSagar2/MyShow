73ec197faa68ac463e99b6d79fd37373acbcf5bd
Sharmasagar2 <sagarkush2003@gmail.com>
2025-06-16 13:19:09 +0530

    added the favorite cast list

---
+++ src/components/DateSelect.jsx
@@ -0,0 +1,45 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import BlurCircle from './BlurCircle'
+import { ChevronLeftIcon, ChevronRightIcon} from 'lucide-react'
+
+const DateSelect = ({dateTime,id}) => {
+    console.log(dateTime)
+  return (
+    <div id='dateSelect' className='pt-30'>
+        <div className='flex flex-col md:flex-row items-center justify-between gap-10
+        relative p-8 bg-primary/10 border border-primary/20 rounded-lg'>
+
+            <BlurCircle top='-100px' left='-100px'/>
+            <BlurCircle top='100px' right='0px'/>
+            <div>
+                <p className='text-lg font-semibold'>Choose Date</p>
+                <div className='flex items-center gap-4 text-sm mt-5'>
+                    <ChevronLeftIcon className='w-7 h-7'/>
+                    <span className='grid grid-cols-3 md:flex flex-wrap md:max-w-lg gap-4'>
+                        {Object.keys(dateTime).map((date)=>(
+                            <button key={date} className='flex flex-col items-center justify-center h-14 w-14 aspect-square rounded cursor-pointer'>
+                                <span className='text-sm font-semibold'>{new Date(date).getDate()}</span>
+                                <span className='text-xs'>{new Date(date).toLocaleDateString("en-US",
+                                    {month:"short"}
+                                )}</span>
+                            </button>
+                        ))}
+                    </span>
+                    <ChevronRightIcon className='w-7 h-7'/>
+
+                </div>
+            </div>
+
+            <button className='bg-primary text-white px-8 py-2 mt-6 rounded hover:bg-primary/90 transition-all cursor-pointer'>
+                Book Now
+            </button>
+
+        </div>
+
+      
+    </div>
+  )
+}
+
+export default DateSelect
